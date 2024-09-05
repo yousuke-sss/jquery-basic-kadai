3ad561bc89cddad8d1f2e5257b27c887f3f1f961
yousuke-sss <you.sekine130702@outlook.jp>
2024-09-05 23:43:37 +0900

    kadai_004

---
+++ kadai_004/event.js
@@ -0,0 +1,13 @@
+$(window).on('load', function() {
+    console.log('loadイベントが発生しました');
+  });
+  
+  $(function() {
+    $(window).on('scroll', function() {
+      console.log('scrollイベントが発生しました');
+    });
+  });
+  
+
+    
+   
